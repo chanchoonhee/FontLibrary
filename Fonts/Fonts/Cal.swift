33947476148070fa054020b5549a61c828738012
Chan Choon Hee <c.choon.hee@gmail.com>
2017-02-13 16:12:08 +0800

    removed empty file

--- Fonts/Fonts/Cal.swift
+++ Fonts/Fonts/Cal.swift
@@ -18,21 +18,3 @@ var arial14 = UIFont(name: "ArialMT", size: 14)
 var arial60 = UIFont(name: "ArialMT", size: 60)
 var arialBold12 = UIFont(name: "Arial-BoldMT", size: 12)
 var arialBold16 = UIFont(name: "Arial-BoldMT", size: 16)
-//var colort = UIColor(colorLiteralRed: 100, green: 0, blue: 0, alpha: 1.0)
-//var text = "HELLO"
-//var multipleAttributes : [String:Any] = [NSFontAttributeName: arial12!]
-
-//class Cal {
-   
-//    func setTextAttributes(_ font: UIFont, _ color: UIColor, _ text: AnyObject ) -> NSMutableAttributedString{
-//        if let text: UILabel = (text as! UILabel) {
-//            let content = text.text
-//            let attributes: [String:Any] = [
-//                NSFontAttributeName: font,
-//                NSForegroundColorAttributeName: color]
-//            let attributedString = NSMutableAttributedString(string: content!, attributes: attributes)
-//            return attributedString
-//        }
-//       
-//    }
-//}
